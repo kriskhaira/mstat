b0a3c2a8dadd8993e7e6cc3ab577a1903a40f2be
dirk.eisenberg <dirk.eisenberg@71f73456-be1d-11de-a0e9-83d03c287164>
2009-10-23 16:13:51 +0000

    added GPL information into src code

--- mstat/MSMainWindowController.m
+++ mstat/MSMainWindowController.m
@@ -1,10 +1,16 @@
 //
 //  MSMainWindowController.m
-//  mstat
 //
-//  Created by dirk.eisenberg on 10/20/09.
-//  Copyright 2009 private. All rights reserved.
-//
+// This file is part of the mstat project and can be redistributed
+// and modified with the terms of the General Public Licence V2 (GPL/V2).
+// 
+// This file is provided "AS IS" with NO WARRANTY OF ANY KIND,
+// INCLUDING THE WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE. Dirk Eisenberg reserves all rights not expressly
+// granted herein.
+// 
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
 #import "MSMainWindowController.h"
 
